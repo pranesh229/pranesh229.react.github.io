e8493e63bd78e1744c983300bb597ebd4ade7501
Pranesh Hanumanth <praneshhanumanth@Praneshs-MBP.home>
2019-07-28 11:59:55 -0400

    Initial commit

---
+++ precache-manifest.1cd549ac0f11efb6e3d0d59bce58a1c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94c174a696de5b882834511bd9735a5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "184e01b398b5cacac48c",
+    "url": "/static/css/main.4da78be6.chunk.css"
+  },
+  {
+    "revision": "c3052a9743e8d648110c",
+    "url": "/static/js/2.88063759.chunk.js"
+  },
+  {
+    "revision": "184e01b398b5cacac48c",
+    "url": "/static/js/main.0e5f415b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
